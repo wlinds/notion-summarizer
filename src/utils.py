93e33f0c6ae80021841910fb03d1e896035f3a6a
William Akilles Lindstedt <akilles.dev@gmail.com>
2024-08-17 17:25:38 +0200

    cleanup, rename, add plot

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,17 @@ import json
 import pandas as pd
 import datetime
 
+from plots import get_plot
+
+# TODO Add error handling for property "Time" missing 
+def get_payload(property_name="Time", week="past_week"):
+    payload = {
+    "filter": {
+        "property": property_name,
+        "date" : { week : {} }}}
+
+    return payload
+
 def get_current_week():
     return datetime.datetime.now().isocalendar()[1]
 
@@ -13,8 +24,8 @@ def get_email_details(json_file_path=None):
     try:
         with open(file_path, 'r') as f:
             email_details = json.load(f)
-        if not all(key in email_details for key in ['sender', 'receiver', 'subject', 'body']):
-            raise KeyError(f"The JSON {file_path} must contain 'sender', 'receiver', 'subject' and 'body' keys.")
+        if not all(key in email_details for key in ['sender', 'receiver', 'subject', 'body', 'file_name']):
+            raise KeyError(f"The JSON {file_path} must contain 'sender', 'receiver', 'subject', 'body' and 'file_name' keys.")
     
     except FileNotFoundError:
         print(f"Error: {file_path} was not found.")
@@ -28,7 +39,7 @@ def get_email_details(json_file_path=None):
 
     return email_details
 
-def format_excel(csv, column_name):
+def get_df(csv, column_name):
     # df = pd.read_csv(csv)
     df = pd.DataFrame(csv)
     
@@ -36,7 +47,8 @@ def format_excel(csv, column_name):
     
     start_times = pd.to_datetime(start_end_times[0])
     end_times = pd.to_datetime(start_end_times[1])
-    
+
+
     df['StartDate'] = start_times.dt.day
     df['StartMonth'] = start_times.dt.month
     df['StartYear'] = start_times.dt.year
@@ -46,14 +58,17 @@ def format_excel(csv, column_name):
     df['EndYear'] = end_times.dt.year
     df['EndTime'] = end_times.dt.strftime('%H:%M')
     
+    plot = get_plot(df, start_times, end_times)
+    df.drop(columns=["StartDateTime", "EndDateTime", "Duration", "Day", "y" ])
+
     df = df.drop(columns=[column_name])
-    
+
     column_order = ['Customer', 'Tags', 'StartDate', 'StartMonth', 'StartYear', 'StartTime', 'EndDate', 'EndMonth', 'EndYear', 'EndTime', 'Note', 'Tim']
     df = df[column_order + [col for col in df.columns if col not in column_order]]
 
     df = df.sort_values(by=['StartYear', 'StartMonth', 'StartDate', 'StartTime'])
     
-    return df
+    return df, plot
 
 
 if __name__ == "__main__":
