2f32329cf97561bf0fa358ca1fd7a258a2197594
William Akilles Lindstedt <akilles.dev@gmail.com>
2024-08-17 17:24:55 +0200

    cleanup, handle multiple attachments

--- src/send_mail.py
+++ src/send_mail.py
@@ -1,41 +1,42 @@
+import os
 import smtplib
-from email.message import EmailMessage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.utils import formatdate
 from email import encoders
-import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
-def write_email(sender, receiver, subject, body, filename=None, is_html=False):
+def write_email(sender, receiver, subject, body, filenames=None, is_html=False):
     msg = MIMEMultipart()
     msg.attach(MIMEText(body, 'html' if is_html else 'plain'))
     
-
     msg['From'] = sender
     msg['To'] = receiver
     msg['Subject'] = subject
     msg['Date'] = formatdate(localtime=True)
 
-    if filename:
-        with open(filename, "rb") as f:
-            part = MIMEBase('application', 'octet-stream')
-            part.set_payload(f.read())
-            encoders.encode_base64(part)
-            part.add_header('Content-Disposition', f"attachment; filename= {filename}")
-            msg.attach(part)
-
-    # return sender, msg
+    if filenames:
+        for filename in filenames:
+            with open(filename, "rb") as f:
+                part = MIMEBase('application', 'octet-stream')
+                part.set_payload(f.read())
+                encoders.encode_base64(part)
+                part.add_header('Content-Disposition', f"attachment; filename= {filename}")
+                msg.attach(part)
 
     if sender[-9::] == 'gmail.com':
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
             smtp.login(sender, os.getenv('EMAIL_APP_PASSWORD'))
             smtp.send_message(msg)
+            return 1
     else:
         print("Your email provider is not supported. Check send_email.py to configure SSL")
+        
+    return 0
 
 if __name__ == "__main__":
     pass
