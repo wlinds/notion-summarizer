559f92473e2868ef00aec370339a6d9d9ac5ccd7
William Akilles Lindstedt <akilles.dev@gmail.com>
2024-08-19 19:50:27 +0200

    cleanup, improve structure

--- src/main.py
+++ src/main.py
@@ -1,47 +1,43 @@
-import os, sys, json, time, datetime
+import os, sys, json, time
+from datetime import datetime as dt
 
 from api import *
-from utils import get_email_details, get_df
-from send_mail import write_email
+from utils import get_email_details, get_df, cleanup_files
+from send_mail import compose_and_send
+from model import get_basic_summary
 
-load_dotenv()
+def generate_report(data, datetime_col, plot_type, file_name):
+    df, plot = get_df(data, datetime_col, plot_type)
+
+    if plot:
+        plot.savefig(file_name + ".png")
+
+    df.to_excel(file_name + ".xlsx", index=False)
+
+    return [file_name + ".xlsx", file_name + ".png"] if plot else [file_name + ".xlsx"]
+
 
 def main():
     start_timer = time.time()
-    
     datetime_col = "Time"
-    ed = get_email_details()
-    ed['week'] = datetime.datetime.now().isocalendar()[1] - 1
-    file_name = f"{ed['file_name']}_{ed['week']}" # Adjusted filename to past week
+    email_details = get_email_details()
+    email_details["week"] = dt.now().isocalendar()[1]
 
+    file_name = f"{email_details['file_name']}_{email_details['week']}"
     data = fetch(DB_ID, get_payload(property_name=datetime_col, week="past_week"))
 
-    if data:
-        formatted_rows = process_data(data)
-    else: return
+    if not data: return
+    
+    clean_data = process_data(data)
+    attachments = generate_report(clean_data, datetime_col, "schedule", file_name)
 
-    df, plot = get_df(formatted_rows, datetime_col, "schedule")
+    email_details['elapsed'] = time.time() - start_timer
+    email_details['body'] += get_basic_summary(clean_data, "Note")
 
-    if plot:
-        plot.savefig(file_name+".png")
-
-    df.to_excel(file_name+".xlsx", index=False)
-
-    attachments = [file_name+".xlsx", file_name+".png"] if plot else file_name+".xlsx"
-
-    ed['elapsed'] = time.time() - start_timer
-    if write_email(ed, attachments, is_html=True) == True:
-        print(f"""• Notion-Summarizer | {datetime.datetime.now()} | Email "{ed['subject']} {ed['week']}" successfully sent to {ed['receiver']}.""")
-
-        try:
-            os.remove(file_name+".xlsx")
-            if plot:
-                os.remove(file_name+".png")
-        except OSError as e:
-            print(f"Error deleting file: {e}")
-    else:
-        print("Could not send email. Files have been saved locally instead.")
+    if compose_and_send(email_details, attachments):
+        cleanup_files(attachments)
 
 
 if __name__ == "__main__":
+    load_dotenv()
     main()
